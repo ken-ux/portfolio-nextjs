66824c9e7d320147675105968479bd430a2686d1
Kenny Nguyen <sokenns@gmail.com>
2022-01-08 16:13:53 -0800

    Add logo and href to links

--- components/ResponsiveAppBar.js
+++ components/ResponsiveAppBar.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Image from 'next/image';
 import Link from '../src/Link';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -9,8 +10,6 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import MenuItem from '@mui/material/MenuItem';
-
-const pages = ['Home', 'About', 'Resume'];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState();
@@ -29,23 +28,37 @@ const ResponsiveAppBar = () => {
         <Toolbar disableGutters>
 
           {/* Desktop View */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, flexGrow: 1 }}
+          <Box
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, flexGrow: 1,
+            transition: 'opacity 170ms ease-in-out', '&:hover': { opacity: "50%", transition: 'opacity 170ms ease-in-out' } }}
+            component={Link} href="/"
           >
-            LOGO
-          </Typography>
+              <Image src="/icon.svg" height="50px" width="50px" />
+          </Box>
 
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
+            {/* {pages.map((page) => (
               <Link href="/" key={page} sx={{ textDecoration: "none", my: 2, mx: 2, display: 'block' }}>
                 <Typography variant="h6" color="white" sx={{ transition: 'opacity 170ms ease-in-out', '&:hover': { opacity: "60%", transition: 'opacity 170ms ease-in-out' } }}>
                   {page}
                 </Typography>
               </Link>
-            ))}
+            ))} */}
+            <Link href="/" sx={{ textDecoration: "none", my: 2, mx: 2, display: 'block' }}>
+              <Typography variant="h6" color="white" sx={{ transition: 'opacity 170ms ease-in-out', '&:hover': { opacity: "60%", transition: 'opacity 170ms ease-in-out' } }}>
+                Home
+              </Typography>
+            </Link>
+            <Link href="/about" sx={{ textDecoration: "none", my: 2, mx: 2, display: 'block' }}>
+              <Typography variant="h6" color="white" sx={{ transition: 'opacity 170ms ease-in-out', '&:hover': { opacity: "60%", transition: 'opacity 170ms ease-in-out' } }}>
+                About
+              </Typography>
+            </Link>
+            <Link href="https://drive.google.com/file/d/1tHtYJSEmKTkuUpOlRGGnazmPU2zKWr7S/view" target="_blank" rel="noopener" sx={{ textDecoration: "none", my: 2, mx: 2, display: 'block' }}>
+              <Typography variant="h6" color="white" sx={{ transition: 'opacity 170ms ease-in-out', '&:hover': { opacity: "60%", transition: 'opacity 170ms ease-in-out' } }}>
+                Resume
+              </Typography>
+            </Link>
           </Box>
 
           {/* Mobile View */}
@@ -78,11 +91,20 @@ const ResponsiveAppBar = () => {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
+              {/* {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography color="black" textAlign="center">{page}</Typography>
                 </MenuItem>
-              ))}
+              ))} */}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography color="black" textAlign="center" underline="none" component={Link} href="/">Home</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography color="black" textAlign="center" underline="none" component={Link} href="/about">About</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography color="black" textAlign="center" underline="none" component={Link} href="https://drive.google.com/file/d/1tHtYJSEmKTkuUpOlRGGnazmPU2zKWr7S/view">Resume</Typography>
+              </MenuItem>
             </Menu>
           </Box>
           <Typography
