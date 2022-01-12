f71365fd9b8fa865393ed241ef93fa1c55da3d4d
Kenny Nguyen <sokenns@gmail.com>
2022-01-12 02:39:17 -0800

    Add anchor link

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,9 +6,9 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import CardActionArea from "@mui/material/CardActionArea";
 import Box from "@mui/material/Box";
+import Link from "next/link";
 
 export default function ProjectCard(props) {
-
   return (
     <Grid item xs={12} md={6}>
       <Card
@@ -25,39 +25,41 @@ export default function ProjectCard(props) {
           alt="project card"
           sx={{ display: "block", width: "100%", height: "auto" }}
         />
-        <CardActionArea
-          sx={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            height: "100%",
-            width: "100%",
-            opacity: 0,
-            transition: ".5s ease",
-            backgroundColor: "rgba(255,255,255,0.95)",
-            color: "black",
-            "&:hover": { opacity: 1 },
-          }}
-        >
-          <CardContent>
-            <Box
-              //   display={"flex"}
-              //   flexDirection={"column"}
-              //   alignItems={"center"}
-              //   justifyContent={"center"}
-              sx={{ px: 5 }}
-            >
-              <Typography variant="h4" component="h2">
-                <strong>{props.title}</strong>
-              </Typography>
-              <Typography>{props.text}</Typography>
-              <br/>
-              <Typography variant="subtitle">{props.tags}</Typography>
-            </Box>
-          </CardContent>
-        </CardActionArea>
+        <Link href={props.link}>
+          <CardActionArea
+            sx={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: "100%",
+              width: "100%",
+              opacity: 0,
+              transition: ".5s ease",
+              backgroundColor: "rgba(255,255,255,0.95)",
+              color: "black",
+              "&:hover": { opacity: 1 },
+            }}
+          >
+            <CardContent>
+              <Box
+                //   display={"flex"}
+                //   flexDirection={"column"}
+                //   alignItems={"center"}
+                //   justifyContent={"center"}
+                sx={{ px: 5 }}
+              >
+                <Typography variant="h4" component="h2">
+                  <strong>{props.title}</strong>
+                </Typography>
+                <Typography>{props.text}</Typography>
+                <br />
+                <Typography variant="subtitle">{props.tags}</Typography>
+              </Box>
+            </CardContent>
+          </CardActionArea>
+        </Link>
       </Card>
     </Grid>
   );
