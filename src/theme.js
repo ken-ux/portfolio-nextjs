9f7dad11ef71e6c44916d8cc38c3c985187611dc
Kenny Nguyen <sokenns@gmail.com>
2022-08-12 20:16:48 -0700

    Change primary theme color

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { red } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#0C80EC",
     },
     secondary: {
       main: "#19857b",
